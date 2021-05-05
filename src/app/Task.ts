e2fd3ff2b626fa13b6ee0d28438952a2f8ea6a97
unknown <ibmjavanh14@iiht.tech>
2021-05-05 12:35:34 +0530

    task form frontend,header and footer components created

---
+++ src/app/Task.ts
@@ -0,0 +1,17 @@
+import { Type } from '@angular/core';
+import { TYPE } from './TYPE';
+import { STATUS } from './STATUS';
+import { OCCURRENCE  } from './OCCURRENCE';
+
+
+export class Task{
+  name:string="Task A";
+  priority:number;
+  startDate:Date;
+  endDate:Date;
+  parent:String;
+  status:STATUS;
+  occurrence:OCCURRENCE;
+  type:TYPE;
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }  from "@angular/forms";
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { TaskFormComponent } from'./task-form/task-form.component';
+const appRoutes: Routes= [
+  {path:'create',component: TaskFormComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TaskFormComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [RouterModule.forRoot(
+    appRoutes,
+    {enableTracing:true}),
+    BrowserModule,FormsModule,HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
